1eb512141c0abc226e857cad80762010c267ce63
g12918ka <g12918ka@s30428.tsuda.ac.jp>
2014-04-11 11:14:59 +0900

    新規にHellowWorld1を作成しました。

--- src/hellowworld1/HellowWorld1.java
+++ src/hellowworld1/HellowWorld1.java
@@ -15,6 +15,6 @@ public class HellowWorld1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HellowWorld");
+        System.out.println("Hellow World");
     }
 }
